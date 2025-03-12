13cb6c1421b0dd9fa93cd963b5083c447b56a8dc
poojasharma3105 <poojash3105@gmail.com>
2025-03-12 15:53:08 +0530

    Increase chunk size warning limit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  build: {
+    chunkSizeWarningLimit: 1000, // Set limit in KB (1000 KB = 1 MB)
+  },
 })
