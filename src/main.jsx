860b0c13486d658f569404f53d3318f1f7eed089
poojasharma3105 <poojash3105@gmail.com>
2025-03-15 14:18:11 +0530

    Added redirects file for React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_REACT_APP_GOOGLE_AUTH_CLIENT_ID}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+    <App />
     </GoogleOAuthProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
