05b1491152018f800dbf2d62d46fb554773a4384
poojasharma3105 <poojash3105@gmail.com>
2025-03-15 14:27:17 +0530

    router updation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import CreateTrip from './pages/CreateTrip';
 import Header from './components/Header';
 import Hero from './components/Hero';
@@ -9,31 +9,28 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <div className="App flex flex-col min-h-screen">
-        <Header />
-        <Toaster />
-        <div className="flex-grow">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <section>
-                    <Hero />
-                  </section>
-                </>
-              }
-            />
-            <Route path="/create-trip" element={<CreateTrip />} />
-            <Route path="/trip/:tripId" element={<ViewTrip />} />
-          </Routes>
-        </div>
-        <Footer />
+    <div className="App flex flex-col min-h-screen">
+      <Header />
+      <Toaster />
+      <div className="flex-grow">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <section>
+                  <Hero />
+                </section>
+              </>
+            }
+          />
+          <Route path="/create-trip" element={<CreateTrip />} />
+          <Route path="/trip/:tripId" element={<ViewTrip />} />
+        </Routes>
       </div>
-    </Router>
+      <Footer />
+    </div>
   );
 }
 
-
-export default App
+export default App;
